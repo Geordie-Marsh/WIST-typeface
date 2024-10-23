e753b8f3128b3f6b1f697537d302516376183659
Geordie <geordiem@student.unimelb.edu.au>
2024-10-24 00:01:42 +1100

    Adding in a way to return to home

--- src/pages/ModeBase.jsx
+++ src/pages/ModeBase.jsx
@@ -1,12 +1,18 @@
 // Imports
-	
-	
+	// Importing components
+	import { Link } from 'react-router-dom';
+
+	// Importing assets
+	import * as Icons from '../components/Icons';
 
 
 
 export default function ModeBase({ children }) {
 	return (
 		<div className="ModeBase">
+			<div className="ModeBase-back">
+				<Link to="/"><Icons.PageBackArrow className={ "Icon--small" } /></Link>
+			</div>
 			{ children }
 		</div>
 	)
